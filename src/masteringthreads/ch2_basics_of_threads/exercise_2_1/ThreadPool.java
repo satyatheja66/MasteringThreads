9c647f0b3577a3c6c6a315cf76a760d58e881526
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-10-26 18:26:25 +0200

    turned on JCiP annotation checking

--- src/masteringthreads/ch2_basics_of_threads/exercise_2_1/ThreadPool.java
+++ src/masteringthreads/ch2_basics_of_threads/exercise_2_1/ThreadPool.java
@@ -1,12 +1,34 @@
 package masteringthreads.ch2_basics_of_threads.exercise_2_1;
 
+import net.jcip.annotations.*;
+
+import java.util.*;
+
 public class ThreadPool {
+    private static final Object nextGroupNumberMonitor = new Object();
+    @GuardedBy("nextGroupNumberMonitor")
+    private static int nextGroupNumber = 1;
+    private final int groupNumber;
+
+    {
+        synchronized (nextGroupNumberMonitor) {
+            groupNumber = nextGroupNumber++;
+        }
+    }
+
     // Create a thread group field
+    private final ThreadGroup group = new ThreadGroup("thread-pool-group-" + groupNumber);
     // Create a LinkedList field containing Runnable
+    @GuardedBy("jobs")
+    private final LinkedList<Runnable> jobs = new LinkedList<>();
     // Hint: Since LinkedList is not thread-safe, we need to synchronize it
 
     public ThreadPool(int poolSize) {
         // create several Worker threads in the thread group
+        for (int i = 0; i < poolSize; i++) {
+            Worker worker = new Worker(group, "worker-" + groupNumber + "-" + (i + 1));
+            worker.start();
+        }
     }
 
     private Runnable take() throws InterruptedException {
