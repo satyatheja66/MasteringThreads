1d7b038971d6f2ca3f33d88461a2d7079fa0771e
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-10-26 16:37:38 +0200

    cost of creating threads

---
+++ src/playground/ThreadCreationCost.java
@@ -0,0 +1,31 @@
+package playground;
+
+import java.util.*;
+
+public class ThreadCreationCost {
+    private static volatile boolean running = true;
+    private static Object object;
+    public static void main(String... args) throws InterruptedException {
+        Timer timer = new Timer(true);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                running = false;
+            }
+        }, 1000);
+
+        Runnable task = () -> {
+//            System.out.println("Hello Safari Students");
+        };
+
+        long count = 0;
+        while(running) {
+            Thread thread = new Thread(task);
+            thread.start();
+            thread.join();
+//            object = new byte[500 * 1024];
+            count++;
+        }
+        System.out.println("count = " + count);
+    }
+}
