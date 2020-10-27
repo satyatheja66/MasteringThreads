cce62d03067be54dc6392ce80ccf5bff9fd0b9c2
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-10-27 17:40:11 +0200

    ExecutorService examples done

--- src/playground/ExecutorExamples.java
+++ src/playground/ExecutorExamples.java
@@ -4,11 +4,37 @@ import java.util.concurrent.*;
 
 public class ExecutorExamples {
     public static void main(String... args) {
-        ExecutorService cached = Executors.newCachedThreadPool();
-        ExecutorService fixed = Executors.newFixedThreadPool(10);
-        ExecutorService scheduled = Executors.newScheduledThreadPool(10);
-        ExecutorService single = Executors.newSingleThreadExecutor();
-        ExecutorService singleScheduled = Executors.newSingleThreadScheduledExecutor();
-        ExecutorService workStealing = Executors.newWorkStealingPool();
+        ExecutorService cached = new ThreadPoolExecutor(
+            0, Integer.MAX_VALUE,
+            60L, TimeUnit.SECONDS,
+            new SynchronousQueue<>());
+        ExecutorService limitedCached = new ThreadPoolExecutor(
+            0, 1000,
+            60L, TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            new ThreadPoolExecutor.CallerRunsPolicy());
+
+        ExecutorService fixed = new ThreadPoolExecutor(
+            10, 10,
+            0L, TimeUnit.MILLISECONDS, // irrelevant
+            new LinkedBlockingQueue<>()); // unbounded queue
+
+        ExecutorService pointless = new ThreadPoolExecutor(
+            10, 100,
+            1L, TimeUnit.MINUTES, // irrelevant
+            new LinkedBlockingQueue<>()); // unbounded queue
+
+        ExecutorService scheduled = new ScheduledThreadPoolExecutor(10);
+
+        ExecutorService sortOfSingle = Executors.newFixedThreadPool(1);
+        ((ThreadPoolExecutor)sortOfSingle).setCorePoolSize(2);
+
+        ExecutorService single = Executors.newSingleThreadExecutor(); // cannot inline
+        ExecutorService singleScheduled = Executors.newSingleThreadScheduledExecutor(); // cannot inline
+
+        ExecutorService workStealing = new ForkJoinPool
+            (Runtime.getRuntime().availableProcessors(),
+                ForkJoinPool.defaultForkJoinWorkerThreadFactory,
+                null, true);
     }
 }
