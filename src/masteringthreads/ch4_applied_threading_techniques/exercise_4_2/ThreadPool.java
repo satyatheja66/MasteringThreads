567f3d657cda21bbcfed665ad355960cf9cf70ff
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-10-27 18:28:26 +0200

    exercise 4.2 done

--- src/masteringthreads/ch4_applied_threading_techniques/exercise_4_2/ThreadPool.java
+++ src/masteringthreads/ch4_applied_threading_techniques/exercise_4_2/ThreadPool.java
@@ -4,48 +4,25 @@ import java.util.concurrent.*;
 
 // TODO: Replace inner workings of ThreadPool with ExecutorService
 public class ThreadPool {
-    private final ThreadGroup group = new ThreadGroup("thread pool");
-    private final BlockingQueue<Runnable> runQueue = new LinkedBlockingQueue<>();
-    private volatile boolean running = true;
+    private final ExecutorService pool;
+    private final LinkedBlockingQueue<Runnable> workQueue =
+        new LinkedBlockingQueue<>();
 
     public ThreadPool(int poolSize) {
-        for (int i = 0; i < poolSize; i++) {
-            var worker = new Worker(group, "Worker" + i);
-            worker.start();
-        }
-    }
-
-    private Runnable take() throws InterruptedException {
-        return runQueue.take();
+        pool = new ThreadPoolExecutor(poolSize, poolSize,
+            0L, TimeUnit.MILLISECONDS,
+            workQueue);
     }
 
     public void submit(Runnable job) {
-        runQueue.add(job);
+        pool.submit(job);
     }
 
     public int getRunQueueLength() {
-        return runQueue.size();
+        return workQueue.size();
     }
 
     public void shutdown() {
-        running = false;
-        group.interrupt();
-    }
-
-    private class Worker extends Thread {
-        public Worker(ThreadGroup group, String name) {
-            super(group, name);
-        }
-
-        public void run() {
-            while (running && !isInterrupted()) {
-                try {
-                    take().run();
-                } catch (InterruptedException e) {
-                    interrupt();
-                    break;
-                }
-            }
-        }
+        pool.shutdownNow();
     }
 }
